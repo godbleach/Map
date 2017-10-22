867e9c37902c48d92aa1bf518ea15605004b974e
godbleach <god_bleach@hotmail.co.th>
2017-10-22 13:23:10 +0700

    add user เข้า DB
    -แต่ยังไม่ได้ลบ URL กะ Title ออก

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,12 +1,12 @@
-Template.postSubmit.onCreated(function() {  
+Template.postSubmit.onCreated(function() {
   Session.set('postSubmitErrors', {});
 });
 
-Template.postSubmit.helpers({  
-  errorMessage: function(field) {    
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
     return Session.get('postSubmitErrors')[field];
-  },  
-  errorClass: function (field) {    
+  },
+  errorClass: function (field) {
     return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
   }
 });
@@ -18,26 +18,34 @@ Template.postSubmit.events({
 
     var post = {
       url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      title: $(e.target).find('[name=title]').val(),
+      userName : $(e.target).find('[name=userName]').val(),
+      firstName : $(e.target).find('[name=firstName]').val(),
+      lastName : $(e.target).find('[name=lastName]').val(),
+      bloodType : $(e.target).find('[name=bloodType]').val(),
+      faculty : $(e.target).find('[name=faculty]').val(),
+      major : $(e.target).find('[name=major]').val(),
+      emerCell : $(e.target).find('[name=emerCell]').val()
+
     };
 
-    var errors = validatePost(post);    
-    if (errors.title || errors.url)      
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
       return Session.set('postSubmitErrors', errors);
 
-    Meteor.call('postInsert', post, function(error, result) {      
-    
-    // display the error to the user and abort      
+    Meteor.call('postInsert', post, function(error, result) {
+
+    // display the error to the user and abort
     if (error)
-      return throwError(error.reason);        
-    	// return alert(error.reason);      
-    
-	// show this result but route anyway      
-	if (result.postExists)     
-    throwError('This link has already been posted');   
+      return throwError(error.reason);
+    	// return alert(error.reason);
+
+	// show this result but route anyway
+	if (result.postExists)
+    throwError('This link has already been posted');
 		// alert('This link has already been posted');
 
-    // Router.go('postPage', {_id: result._id});      
+    // Router.go('postPage', {_id: result._id});
 	});
 
 	Router.go('/map');
